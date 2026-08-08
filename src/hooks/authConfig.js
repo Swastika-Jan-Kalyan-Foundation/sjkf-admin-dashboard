08969aeb6094d4560ab38d4c4d958f347fdceb36
Gyan Shresth(Kunal) <gyan0304@gmail.com>
2026-08-08 13:51:56 +0530

    del: deleted harcoded hash

--- src/hooks/authConfig.js
+++
@@ -1,6 +0,0 @@
-export const AUTH_CONFIG = {
-  usernameHash: "9dc14894c1f9b13c44cd09a052c04c022254ce2df04fd7a005857a600a9fd336",
-  passwordHash: "8e805f80e7425b035c604caa7bd5ed35355b5df3a9bc45773d7f203648cdec76",
-  sessionKey: "sjkf_admin_session",
-  sessionDuration: 8 * 60 * 60 * 1000, 
-};
